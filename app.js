db9df630c04fd8f9d55a116a1abd6d2d9b323674
rafii <moideenrafihpa@gmail.com>
2023-01-15 22:00:09 +0530

    db connection done

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var hbs = require("express-handlebars");
-var fileUpload = require('express-fileupload');
+var fileUpload = require("express-fileupload");
+var db = require("./config/connection");
 
 var userRouter = require("./routes/user");
 var adminRouter = require("./routes/admin");
@@ -30,6 +31,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(fileUpload());
+db.connect((err) => {
+  if (err) console.log("Connection Error" + err);
+  else console.log("Database Connected Succesfully to port 27017");
+});
 
 app.use("/", userRouter);
 app.use("/admin", adminRouter);
--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,19 @@
-const mongoClient = require('mongodb').MongoClient;
+const mongoClient = require("mongodb").MongoClient;
+
+const state = {
+  db: null,
+};
+
+module.exports.connect = (done) => {
+  const url = "mongodb://localhost:27017";
+  const dbName = "shopping";
+  mongoClient.connect(url, (err, data) => {
+    if (err) return done(err);
+    state.db = data.db(dbName);
+    done();
+  });
+};
+
+module.exports.get = () => {
+  return state.db;
+};
